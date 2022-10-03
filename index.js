6d654473115492b24d4203d8b90ab8c4b2075cbc
Le Huynh <lhuynh100@gmail.com>
2022-10-03 14:56:10 -0500

    commit message

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => { 
+    let words = tutorial.split(' ') 
+    return words.map(word => { 
+      return word.charAt(0).toUpperCase() + word.slice(1) 
+    }).join(' ');
+
+  })
 }
+
